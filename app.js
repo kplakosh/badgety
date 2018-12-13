38beecef8303c9bdbf7c09edeb30293d2d799f2f
kplakosh <kplakosh@gmail.com>
2018-12-12 19:34:18 -0800

    created object to contain input classes

--- app.js
+++ app.js
@@ -5,14 +5,39 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-    // Some code
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getinput: function() { // public parameter
+            // method which returns three controlers
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
 })();
 
 // GLOBAL CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UIController.getDOMstrings();
+
     var ctrlAddItem = function() {
         // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
 
         // 2. Add the item to budget controller
 
@@ -21,11 +46,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the badget
 
         // 5. Display the badget on the UI
-        console.log('It works');
     }
 
     // some controller
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13) {
