48a0231a78e0e459f90725b567c0b398bae149ad
kplakosh <kplakosh@gmail.com>
2018-12-12 18:31:52 -0800

    event listener added for any key pressed

--- app.js
+++ app.js
@@ -11,9 +11,26 @@ var UIController = (function() {
 // GLOBAL CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
+
+        // 2. Add the item to budget controller
+
+        // 3. Add the item to the budget
+
+        // 4. Calculate the badget
+
+        // 5. Display the badget on the UI
+        console.log('It works');
+    }
+
     // some controller
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        console.log('button was clicked');
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
     });
 
 })(budgetController, UIController);
